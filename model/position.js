a9ae5ddccebf29c3af1d0999d4bb6bd55dbf4e1b
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-11-04 01:18:06 +0530

    Position creation added,

--- model/position.js
+++ model/position.js
@@ -9,4 +9,12 @@ const positionSchema = new Schema({
   },
 });
 
+positionSchema.pre("save", async function (next) {
+  const position = this;
+  if (position.title.length < 5) {
+    next(new Error("position should be atleast 5 charecters."));
+  }
+  next();
+});
+
 module.exports = mongoose.model("Position", positionSchema);
--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,5 +1,6 @@
-import Member from "../model/member";
 import getUserData from "../middleware/auth";
+import Member from "../model/member";
+import Position from "../model/position";
 
 const Mutation = {
   createMember: async (
@@ -20,6 +21,17 @@ const Mutation = {
       console.log(err.message);
       throw err;
     }
+  },
+
+  createPosition: async (parent, { title }, { request }, info) => {
+    const userData = getUserData(request);
+    if (userData.category !== "developer") {
+      throw new Error("only developer can add new member.");
+    }
+
+    const position = new Position({ title });
+    await position.save();
+    return position;
   },
 };
 
