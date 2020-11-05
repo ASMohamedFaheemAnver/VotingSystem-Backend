00ab81a01e4bc136a29b39c32a8b481d11f2681b
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-11-05 10:16:13 +0530

    Get first poll results query added,

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Developer from "../model/developer";
 import Position from "../model/position";
+import Vote from "../model/vote";
 
 import Member from "../model/member";
 import getUserData from "../middleware/auth";
@@ -100,6 +101,97 @@ const Query = {
 
     const positions = await Position.find();
     return positions;
+  },
+
+  /* should learn aggregate in mongoose
+  getFirstPollResult: async (parent, args, { request }, info) => {
+    const userData = getUserData(request);
+
+    // console.log(userData);
+    if (userData.category !== "developer") {
+      throw new Error("only developer can view poll result.");
+    }
+
+    const positions = await Position.find();
+    // console.log(positions);
+    let pollResults = [];
+
+    for (let i = 0; i < positions.length; i++) {
+      const eligible_member_infos = [];
+
+      let pollResult = {
+        position: positions[i],
+        eligible_member_infos: eligible_member_infos,
+      };
+
+      const eligible_members = await Vote.find({
+        position: position[i],
+        meta: "first",
+      });
+    }
+
+    return pollResults;
+  },
+  */
+
+  getFirstPollAllResult: async (parent, args, { request }, info) => {
+    const userData = getUserData(request);
+
+    // console.log(userData);
+    if (userData.category !== "developer") {
+      throw new Error("only developer can view poll result.");
+    }
+
+    const positions = await Position.find();
+    // console.log(positions);
+    let pollResults = [];
+
+    for (let i = 0; i < positions.length; i++) {
+      const eligible_member_infos = [];
+
+      let pollResult = {
+        position: positions[i],
+        eligible_member_infos: eligible_member_infos,
+      };
+
+      let members;
+      const gPosition = positions[i];
+      if (
+        gPosition.eligible_gender.toString() === "M" ||
+        gPosition.eligible_gender.toString() === "F"
+      ) {
+        members = await Member.find({
+          year: gPosition.eligible_year,
+          gender: gPosition.eligible_gender,
+        });
+      } else {
+        members = await Member.find({
+          year: gPosition.eligible_year,
+        });
+      }
+
+      // console.log(members);
+      let eligible_member_info;
+      // console.log(members.length);
+      for (let j = 0; j < members.length; j++) {
+        const received_vote = await Vote.countDocuments({
+          position: gPosition._id,
+          to: members[j]._id,
+          meta: "first",
+        });
+        eligible_member_info = {
+          member: members[j],
+          vote_recieved: received_vote,
+        };
+        // console.log(eligible_member_info);
+        pollResult.eligible_member_infos.push(eligible_member_info);
+
+        // pollResult.eligible_member_infos.push(eligible_member_info);
+      }
+      pollResults.push(pollResult);
+    }
+
+    return pollResults;
   },
 };
 
